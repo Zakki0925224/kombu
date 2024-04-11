f94c7ebb0eb003fd92e2e1a96af272b02c258798
Zakki0925224 <zakki0925224@gmail.com>
2024-04-11 21:35:59 +0900

    [Add]: Added `run` command

---
+++ cmd/run.go
@@ -0,0 +1,69 @@
+package cmd
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"os/exec"
+
+	"github.com/google/subcommands"
+)
+
+type Run struct {}
+
+func (*Run) Name() string { return "run" }
+func (*Run) Synopsis() string { return "run" }
+func (*Run) Usage() string { return "run" }
+func (*Run) SetFlags(f *flag.FlagSet) {
+    // abc
+}
+
+func (t *Run) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+    args := f.Args()
+
+    if len(args) == 0 {
+        fmt.Printf("No commands\n")
+        return subcommands.ExitFailure
+    }
+
+    cmd := exec.Command(args[0], args[1:]...)
+
+    stdout, err := cmd.StdoutPipe()
+    if err != nil {
+        fmt.Println(err)
+        return subcommands.ExitFailure
+    }
+
+    stderr, err := cmd.StderrPipe()
+    if err != nil {
+        fmt.Println(err)
+        return subcommands.ExitFailure
+    }
+
+    err = cmd.Start()
+    if err != nil {
+        fmt.Println(err)
+        return subcommands.ExitFailure
+    }
+    fmt.Printf("Running at pid: %d\n", cmd.Process.Pid)
+
+    scan := bufio.NewScanner(stdout)
+    scanErr := bufio.NewScanner(stderr)
+    go print(scan)
+    go print(scanErr)
+
+    err = cmd.Wait()
+    if err != nil {
+        fmt.Println(err)
+        return subcommands.ExitFailure
+    }
+
+    return subcommands.ExitSuccess
+}
+
+func print(r *bufio.Scanner) {
+    for r.Scan() {
+        fmt.Println(r.Text())
+    }
+}
--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
     subcommands.Register(subcommands.CommandsCommand(), "")
     subcommands.Register(subcommands.FlagsCommand(), "")
     subcommands.Register(subcommands.HelpCommand(), "")
-    subcommands.Register(new(cmd.Test), "")
+    subcommands.Register(new(cmd.Run), "")
     flag.Parse()
 
     ctx := context.Background()
